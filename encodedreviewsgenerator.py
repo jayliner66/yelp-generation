002e25cc6fbdbbaff8a77875c52103e948778a35
Akshaj Kadaveru <akshajk@mit.edu>
2019-12-02 20:26:01 -0500

    encodedreviewsgenerator

---
+++ encodedreviewsgenerator.py
@@ -0,0 +1,18 @@
+import json
+import review_encoding
+####
+def json_readline(file):
+    for line in open(file, mode="r"):
+        yield json.loads(line)
+
+input = []
+output = []
+for review in json_readline("yelp_dataset/review.json"):
+    arr = review_encoding(review)
+    input.append([10015] + review) # START
+    output.append(review + [10016]) # STOP
+
+with open('inputdata.json', 'w') as f:
+    json.dump(input, f)
+with open('outputdata.json', 'w') as f:
+    json.dump(output, f)
