ae4367a204736aa8c1620902c6e3caed57ea6d90
Akshaj Kadaveru <akshajk@mit.edu>
2019-12-03 00:44:30 -0500

    james

--- encodedreviewsgenerator.py
+++ encodedreviewsgenerator.py
@@ -2,6 +2,8 @@ import json
 import numpy as np
 from review_encoding import review_encoding
 from review_encoding import words
+from review_context import make_context
+from review_context import model
 from global_constants import N
 ####
 def json_readline(file):
@@ -10,17 +12,22 @@ def json_readline(file):
 
 input_data = []
 output_data = []
+context = []
 count = 0
 for review in json_readline("yelp_dataset/review.json"):
     encoded_review = review_encoding(review["text"])
+    review_context = make_context(review["text"])
     input_data.append(encoded_review[:-1]) # START
     output_data.append(encoded_review[1:]) # STOP
+    context.append(review_context)
     count += 1
-    if(count == 100):
-        break
+    if(count % 100 == 0):
+        print(count)
 
 
 with open('inputdata.json', 'w') as f:
     json.dump(input_data, f)
 with open('outputdata.json', 'w') as f:
     json.dump(output_data, f)
+with open('context.json', 'w') as f:
+    json.dump(context, f)
