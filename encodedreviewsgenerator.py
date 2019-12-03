12933bc875c52c0399dc77ae9a1fc9e868d5ac33
Akshaj Kadaveru <akshajk@mit.edu>
2019-12-02 20:39:40 -0500

    only runs on 1000

--- encodedreviewsgenerator.py
+++ encodedreviewsgenerator.py
@@ -7,10 +7,14 @@ def json_readline(file):
 
 input = []
 output = []
+count = 0
 for review in json_readline("yelp_dataset/review.json"):
     arr = review_encoding(review)
     input.append([10015] + review) # START
     output.append(review + [10016]) # STOP
+    count += 1
+    if(count == 1000):
+        break
 
 with open('inputdata.json', 'w') as f:
     json.dump(input, f)
